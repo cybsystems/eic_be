d3b0d74ae8f75a43e700f2d5e6ed969f9a80fdfb
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-16 04:18:13 +0530

    minor changes

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -55,9 +55,8 @@ const createUser = async (req, res) => {
 const signInUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await Users.findOne({ email });
+    const user = await Users.findOne({ where: { email } });
     if (!user) return res.status(404).json({ message: "User not found" });
-
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
