e9c857b4ba66bc3c3b15910b2dcf1a8804cede3e
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-17 19:25:14 +0530

    changes added in contractor

--- models/contractor.js
+++ models/contractor.js
@@ -9,6 +9,13 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull:true
+    },
     categoryId: {
       type: DataTypes.INTEGER,
       references: {
@@ -18,28 +25,6 @@ module.exports = (sequelize, DataTypes) => {
       onUpdate: 'CASCADE',
       onDelete: 'SET NULL',
     },
-    // unitId: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   references: {
-    //     model: 'ContractorUnits',
-    //     key: 'id',
-    //   },
-    //   onUpdate: 'CASCADE',
-    //   onDelete: 'SET NULL',
-    // },
-    // contactPerson: {
-    //   type: DataTypes.STRING,
-    //   allowNull: true,
-    // },
-    // contactNumber: {
-    //   type: DataTypes.STRING,
-    //   allowNull: true,
-    // },
-    // address: {
-    //   type: DataTypes.STRING,
-    //   allowNull: true,
-    // },
     createdBy: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -74,7 +59,6 @@ module.exports = (sequelize, DataTypes) => {
 
   Contractor.associate = function(models) {
     Contractor.belongsTo(models.ContractorCategory, { foreignKey: 'categoryId', as: 'category' });
-    Contractor.belongsTo(models.ContractorUnit, { foreignKey: 'unitId', as: 'unit' });
     Contractor.belongsTo(models.UserTable, { as: 'creator', foreignKey: 'createdBy' });
     Contractor.belongsTo(models.UserTable, { as: 'updater', foreignKey: 'updatedBy' });
     Contractor.hasMany(models.Dispatch, { foreignKey: 'contractorUnitAssignmentId' });
