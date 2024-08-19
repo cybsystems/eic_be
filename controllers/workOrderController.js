37c6d63a33b4452645e0a571f1d07eccc52b4bc3
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-19 17:48:10 +0530

    status change api added

--- controllers/workOrderController.js
+++ controllers/workOrderController.js
@@ -46,7 +46,7 @@ exports.getAllWorkOrders = async (req, res) => {
         {
           model: UserTable,
           as: "authorizedUser",
-          attributes: ["username"], // User Authorized by
+          attributes: ["firstName","lastName","email"], // User Authorized by
         },
       ],
     });
@@ -57,3 +57,77 @@ exports.getAllWorkOrders = async (req, res) => {
     res.status(500).json({ message: "Error fetching work orders" });
   }
 };
+
+exports.updateStatus=async(req,res)=>{
+  const { id } = req.params;
+  const { status } = req.body;
+
+  if (!status) {
+    return res.status(400).json({ error: 'Status is required' });
+  }
+
+  try {
+    const workOrder = await WorkOrder.findByPk(id);
+
+    if (!workOrder) {
+      return res.status(404).json({ error: 'Work order not found' });
+    }
+
+    // Update status
+    workOrder.status = status;
+    await workOrder.save();
+
+    res.status(200).json({ message: 'Work order status updated successfully', workOrder });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+exports.getWorkOrderById=async(req,res)=>{
+  try {
+    const { id } = req.params; // Get the ID from the request parameters
+
+    // Find the work order with the given ID and include related models
+    const workOrder = await WorkOrder.findByPk(id, {
+      include: [
+        {
+          model: Project,
+          attributes: ["name"], // Project Name
+        },
+        {
+          model: ContractorUnitAssignment,
+          include: [
+            {
+              model: ContractorUnit,
+              attributes: ["name"], // Unit Name
+            },
+            {
+              model: Contractor,
+              attributes: ["name"], // Contractor Name
+            }
+          ],
+        },
+        {
+          model: UserTable,
+          as: "creator",
+          attributes: ["username","firstName","lastName","email"], // User Authorized by
+        },
+        {
+          model: UserTable,
+          as: "authorizedUser",
+          attributes: ["firstName","lastName","email"], // User Authorized by
+        },
+      ],
+    });
+
+    if (!workOrder) {
+      return res.status(404).json({ message: "Work order not found" });
+    }
+
+    res.json(workOrder);
+  } catch (error) {
+    console.error("Error fetching work order by ID:", error);
+    res.status(500).json({ message: "Error fetching work order by ID" });
+  }
+}
--- routes/workOrders.js
+++ routes/workOrders.js
@@ -5,5 +5,7 @@ const authenticateToken = require('../middleware/auth')
 
 router.post('/', authenticateToken, workOrderController.createWorkOrder);
 router.get('/', authenticateToken, workOrderController.getAllWorkOrders);
+router.get('/:id', authenticateToken, workOrderController.getWorkOrderById);
+router.patch('/:id/status', authenticateToken, workOrderController.updateStatus);
 
 module.exports = router;
