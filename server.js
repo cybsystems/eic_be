746818ac92dff7563e257b838785deae93ae977a
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-24 14:46:47 +0530

    sync is off

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 
 // Start the server and synchronize models
 const PORT = process.env.PORT || 5000;
-sequelize.sync({alert:true});
+// sequelize.sync({alert:true});
 app.listen(PORT, ()=>{
   console.log(`Server is running on port ${PORT}`);
 });
